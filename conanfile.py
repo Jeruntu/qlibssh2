07904ad115b4fb6d049072a773434039720362a9
Jeroen Oomkes <jeruntu@gmail.com>
2020-12-30 11:05:50 +0100

    Conan can now use pre installed Qt or build from source

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conans import ConanFile, CMake
-
+import os
 
 class Qlibssh2Conan(ConanFile):
     name = "qlibssh2"
@@ -10,54 +10,70 @@ class Qlibssh2Conan(ConanFile):
     description = "Qt wrapper for libssh2"
     topics = ("ssh", "ssh2", "qt")
     settings = "os", "compiler", "build_type", "arch"
-    options = {}
-    default_options = {}
+    options = {
+        "local_qt_version": "ANY",
+    }
+    default_options = {
+        "local_qt_version": None,
+    }
     generators = "cmake_paths", "cmake_find_package"
     requires = [
         "libssh2/[>=1.9.0]",
-        "openssl/1.1.1g"
-        # "qt/5.15.2@bincrafters/stable",
+        "openssl/1.1.1g",
     ]
     exports_sources = [
         "src/*",
         "CMakeLists.txt",
-        "cmake/QLibssh2Config.cmake.in"
+        "cmake/QLibssh2Config.cmake.in",
     ]
 
     def config_options(self):
         pass
 
+    def requirements(self):
+        if self.options.local_qt_version:
+            if os.getenv('QTDIR') is None:
+                raise Exception("To use local pre installed Qt, QTDIR env variable has to be defined")
+        else:
+            self.requires("qt/[>=5.15.0 <6.0.0]@bincrafters/stable")
+
     def configure(self):
-        self.options["qt"].shared = True
-        self.options["qt"].commercial = False
-
-        self.options["qt"].opengl = "no"
-        self.options["qt"].openssl = False
-        self.options["qt"].with_vulkan = False
-        self.options["qt"].with_pcre2 = False
-        self.options["qt"].with_glib = False
-        self.options["qt"].with_freetype = False
-        self.options["qt"].with_fontconfig = False
-        self.options["qt"].with_harfbuzz = False
-        self.options["qt"].with_libjpeg = False
-        self.options["qt"].with_libpng = False
-        self.options["qt"].with_sqlite3 = False
-        self.options["qt"].with_mysql = False
-        self.options["qt"].with_pq = False
-        self.options["qt"].with_odbc = False
-        self.options["qt"].with_sdl2 = False
-        self.options["qt"].with_libalsa = False
-        self.options["qt"].with_openal = False
-        self.options["qt"].with_zstd = False
-        self.options["qt"].with_pq = False
-        self.options["qt"].GUI = False
-        self.options["qt"].widgets = False
-
         self.options["libssh2"].shared = False
 
+        if not self.options.local_qt_version:
+            self.options["qt"].shared = True
+            self.options["qt"].commercial = False
+
+            self.options["qt"].opengl = "no"
+            self.options["qt"].openssl = False
+            self.options["qt"].with_vulkan = False
+            self.options["qt"].with_pcre2 = False
+            self.options["qt"].with_glib = False
+            self.options["qt"].with_freetype = False
+            self.options["qt"].with_fontconfig = False
+            self.options["qt"].with_harfbuzz = False
+            self.options["qt"].with_libjpeg = False
+            self.options["qt"].with_libpng = False
+            self.options["qt"].with_sqlite3 = False
+            self.options["qt"].with_mysql = False
+            self.options["qt"].with_pq = False
+            self.options["qt"].with_odbc = False
+            self.options["qt"].with_sdl2 = False
+            self.options["qt"].with_libalsa = False
+            self.options["qt"].with_openal = False
+            self.options["qt"].with_zstd = False
+            self.options["qt"].with_pq = False
+            self.options["qt"].GUI = False
+            self.options["qt"].widgets = False
+
     def build(self):
+        defs = None
+        if self.options.local_qt_version:
+            qtdir = os.getenv('QTDIR').replace('\\', '/')
+            defs = { 'CMAKE_PREFIX_PATH': qtdir }
+
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(defs=defs)
         cmake.build()
 
     def package(self):
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -5,12 +5,23 @@ from conans import ConanFile, CMake, tools
 
 class Qlibssh2TestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "local_qt_version": "ANY",
+    }
+    default_options = {
+        "local_qt_version": None,
+    }
     generators = "cmake_paths", "cmake_find_package"
 
     def build(self):
-        cmake = CMake(self)
         cwd = os.getcwd().replace('\\', '/')
-        cmake.configure(defs={ "CMAKE_PROJECT_PackageTest_INCLUDE": "{}/conan_paths.cmake".format(cwd)} )
+        defs = { "CMAKE_PROJECT_PackageTest_INCLUDE": "{}/conan_paths.cmake".format(cwd) }
+        if self.options.local_qt_version:
+            qtdir = os.getenv('QTDIR').replace('\\', '/')
+            defs['CMAKE_PREFIX_PATH'] = qtdir
+
+        cmake = CMake(self)
+        cmake.configure(defs=defs)
         cmake.build()
 
     def test(self):
